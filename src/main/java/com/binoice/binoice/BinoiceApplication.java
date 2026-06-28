005c9b630c2acf94e719134b1a6182ad4d7f2427
Elijah <einzenheim@yahoo.com>
2026-06-28 19:36:42 -0400

    init

---
+++ src/main/java/com/binoice/binoice/BinoiceApplication.java
@@ -0,0 +1,13 @@
+package com.binoice.binoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinoiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/binoice/binoice/BinoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binoice.binoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
